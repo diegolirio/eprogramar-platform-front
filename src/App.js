f55f7f61f694e6a72aebb1cb00b794f1dcbb90c7
Fabiano Góes <fabianogoes@gmail.com>
2020-09-13 01:03:26 -0300

    set eprogramar colors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,17 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          e-Programar Platform 
+        </h1>
+
+        <h5 className="App-subTitle">
+          Plataforma para estudo de Programação
+        </h5>
       </header>
     </div>
   );
