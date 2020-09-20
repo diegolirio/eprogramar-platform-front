51397203623b98dcb395bcdbd76f6e72699784fb
Felipe Oliver <bispos_lipe@hotmail.com>
2020-09-19 22:57:24 -0300

    feature: put basic style in sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,143 +4,61 @@
     id="accordionSidebar"
   >
     <!-- Sidebar - Brand -->
-    <div class="nav-item">
-      <router-link class="nav-link" to="/"></router-link>
-    </div>
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <router-link to="/" class="sidebar-brand d-flex align-items-center justify-content-center">
       <img
-        class="sidebar-brand-icon"
-        style="height: 45px"
+        class="sidebar-brand-icon logo-sidebar"
         src="../assets/logo-eprogramar.png"
         alt="Logo do eprogramar"
       />
       <div class="sidebar-brand-text mx-3">Nome do Curso</div>
-    </a>
+    </router-link>
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0" />
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-      <a class="nav-link" href="index.html">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span>
-      </a>
-    </li>
+    <div class="lesson-heading">
+      <div class="lesson-heading__number">
+        <small>Aula</small>
+        <span>13</span>
+      </div>
+      <div class="lesson-heading__description">descrição da aula</div>
+    </div>
 
     <!-- Divider -->
     <hr class="sidebar-divider" />
 
     <!-- Heading -->
-    <div class="sidebar-heading">Interface</div>
+    <div class="sidebar-heading">Atividades</div>
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        href="#"
-        data-toggle="collapse"
-        data-target="#collapseTwo"
-        aria-expanded="true"
-        aria-controls="collapseTwo"
-      >
-        <i class="fas fa-fw fa-cog"></i>
-        <span>Components</span>
+      <a class="nav-link" href="#">
+        <i class="far fa-fw fa-file-alt"></i>
+        <span>Instalar ambiente</span>
       </a>
-      <div
-        id="collapseTwo"
-        class="collapse"
-        aria-labelledby="headingTwo"
-        data-parent="#accordionSidebar"
-      >
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header">Custom Components:</h6>
-          <a class="collapse-item" href="buttons.html">Buttons</a>
-          <a class="collapse-item" href="cards.html">Cards</a>
-        </div>
-      </div>
     </li>
-
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        href="#"
-        data-toggle="collapse"
-        data-target="#collapseUtilities"
-        aria-expanded="true"
-        aria-controls="collapseUtilities"
-      >
-        <i class="fas fa-fw fa-wrench"></i>
-        <span>Utilities</span>
-      </a>
-      <div
-        id="collapseUtilities"
-        class="collapse"
-        aria-labelledby="headingUtilities"
-        data-parent="#accordionSidebar"
-      >
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header">Custom Utilities:</h6>
-          <a class="collapse-item" href="utilities-color.html">Colors</a>
-          <a class="collapse-item" href="utilities-border.html">Borders</a>
-          <a class="collapse-item" href="utilities-animation.html">Animations</a>
-          <a class="collapse-item" href="utilities-other.html">Other</a>
-        </div>
-      </div>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider" />
-
-    <!-- Heading -->
-    <div class="sidebar-heading">Addons</div>
 
     <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item active">
-      <a
-        class="nav-link"
-        href="#"
-        data-toggle="collapse"
-        data-target="#collapsePages"
-        aria-expanded="true"
-        aria-controls="collapsePages"
-      >
-        <i class="fas fa-fw fa-folder"></i>
-        <span>Pages</span>
-      </a>
-      <div
-        id="collapsePages"
-        class="collapse show"
-        aria-labelledby="headingPages"
-        data-parent="#accordionSidebar"
-      >
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header">Login Screens:</h6>
-          <a class="collapse-item" href="login.html">Login</a>
-          <a class="collapse-item" href="register.html">Register</a>
-          <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-          <div class="collapse-divider"></div>
-          <h6 class="collapse-header">Other Pages:</h6>
-          <a class="collapse-item" href="404.html">404 Page</a>
-          <a class="collapse-item active" href="blank.html">Blank Page</a>
-        </div>
-      </div>
-    </li>
-
-    <!-- Nav Item - Charts -->
     <li class="nav-item">
-      <a class="nav-link" href="charts.html">
-        <i class="fas fa-fw fa-chart-area"></i>
-        <span>Charts</span>
+      <a class="nav-link" href="#">
+        <i class="far fa-fw fa-play-circle"></i>
+        <span>Instalar ambiente</span>
       </a>
     </li>
 
-    <!-- Nav Item - Tables -->
+    <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-      <a class="nav-link" href="tables.html">
-        <i class="fas fa-fw fa-table"></i>
-        <span>Tables</span>
+      <a class="nav-link" href="#">
+        <i class="fab fa-fw fa-youtube"></i>
+        <span>Instalar ambiente</span>
+      </a>
+    </li>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+      <a class="nav-link" href="#">
+        <i class="far fa-fw fa-file-alt"></i>
+        <span>Instalar ambiente</span>
       </a>
     </li>
 
@@ -157,8 +75,41 @@
 <style lang="scss" scoped>
 .bg-gradient-eprogramar {
   background-color: #007bff;
-  background-image: linear-gradient(180deg, #6D3396 10%, #37005D 100%);
+  background-image: linear-gradient(180deg, #6d3396 10%, #37005d 100%);
   background-size: cover;
+}
+
+.logo-sidebar {
+  height: 45px;
+}
+
+.lesson-heading {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  color: rgba(255, 255, 255, 0.4);
+  text-transform: uppercase;
+  flex-wrap: wrap;
+  font-weight: 700;
+  margin-top: 5px;
+
+  &__number {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+    text-align: center;
+  }
+
+  &__description {
+    vertical-align: middle;
+    text-align: center;
+    font-size: 0.65rem;
+  }
+
+  .sidebar.toggled &__description {
+    display: none;
+  }
 }
 </style>
 
