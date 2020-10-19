be583c23668e9cc94ef76e360cb344cf22642756
Diego Lirio <diegolirio.dl@gmail.com>
2020-10-19 07:59:38 -0300

    refactor clean console.log

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,8 +2,6 @@ import { http } from './config';
 
 export default {
   signin: (email, password) => {
-    let creds = { email, password };
-    console.log(creds);
-    return http.post('auth/signin', creds);
+    return http.post('auth/signin', { email, password }); 
   },
 };
