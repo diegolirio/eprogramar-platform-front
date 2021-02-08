ce688ef35c62c15342b51d742e28e54e8c08076c
Diego Lirio <diegolirio.dl@gmail.com>
2021-02-08 13:37:19 -0300

    Home :: Course Cards' dList

---
+++ src/components/CardCourse.vue
@@ -0,0 +1,118 @@
+<template>
+
+  <!-- <div class="card border-primary">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img :src="tumbnail" class="card-img card__image" alt="Curso" />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body text-left">
+          <h5 class="card-title card__title">{{ name }}</h5>
+          <p class="card-text card__text">
+            This is a wider asdfsdfas dfasdfasdfsdfasd fafds sdfgdfg sdfgsdfg
+            sdsdfg.
+          </p>
+
+          <a
+            v-on:click="$emit('play-course', _id)"
+            class="btn btn-success card__button"
+          >
+            <i class="fas fa-play mr-1"></i>
+            Assistir Curso
+          </a>
+        </div>
+      </div>
+    </div>
+  </div> -->
+
+  <v-card
+
+  >
+    <v-img
+      :src="tumbnail"
+      height="200px"
+      class="grey lighten-2"
+    ></v-img>
+
+    <v-card-title>
+      {{ name }}
+    </v-card-title>
+
+    <v-card-subtitle>
+      This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="success"
+        style="width: 100%;"
+        v-on:click="$emit('play-course', _id)"
+      >
+        Assistir Curso
+      </v-btn>
+    </v-card-actions>
+
+  </v-card>
+
+</template>
+
+<style lang="scss" scoped>
+%truncate_placeholder {
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+//truncate
+@mixin truncate($mx: 250px) {
+  @media (min-width: 768px) {
+    max-width: $mx;
+  }
+  justify-content: space-between;
+  align-items: center;
+  -webkit-box-orient: vertical;
+  display: -webkit-box;
+  @extend %truncate_placeholder;
+}
+
+.card {
+  @media (min-width: 768px) {
+    height: 200px;
+  }
+
+  & > div {
+    height: 100%;
+  }
+
+  &__text {
+    @include truncate;
+    -webkit-line-clamp: 2;
+  }
+
+  &__image {
+    height: 100%;
+    max-height: 200px;
+  }
+
+  &__title {
+    @include truncate;
+    -webkit-line-clamp: 2;
+
+    @media (min-width: 768px) {
+      height: 2.5em;
+    }
+  }
+
+  &__button {
+    @include truncate;
+    -webkit-line-clamp: 1;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: "CardCourse",
+  components: {},
+  props: ['tumbnail', 'name', '_id' ],
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,22 @@
 
     <div>
         <AppBar :navigate="navigate"/>             
-        <!-- <v-main class="">
-            <router-link to="/classroom">
-              <img alt="Vue logo" src="../assets/logo.png">
-            </router-link>
-        </v-main> -->
         <v-main>
           <v-container>
             <v-row>
+
               <v-col
-                v-for="n in 4"
-                :key="n"
+                v-for="course in this.courses" :key="course.id"
                 cols="4"
               >
-                <v-card height="200" to="/classroom"></v-card>
+                <CardCourse
+                  v-on:play-course="playCourse"
+                  :name="course.name"
+                  :tumbnail="course.tumbnail"
+                  :_id="course._id"
+                />                
               </v-col>
+
             </v-row>
           </v-container>
         </v-main>        
@@ -25,14 +26,48 @@
 
 <script>
 import AppBar from '../components/AppBar';
+import storage from "../services/storage";
+import Course from "../services/course";
+import CardCourse from "@/components/CardCourse.vue";
 export default {
   
   name: 'Home',
   components: {
-    AppBar
-  },  
+    AppBar, CardCourse
+  },
+  data: () =>  ({
+      courses: [],
+      userName: "",
+      userEmail: "",
+  }), 
   beforeMount() {
     this.navigate = false; 
-  }
+    this.getCourses();
+  },
+  created() {
+    this.userName = storage.getCurrentUserName();
+    this.userEmail = storage.getCurrentUser();
+  },
+  methods: {
+    getCourses() {
+      Course.getCoursesByUser(storage.getCurrentUser()).then((response) => {
+        this.courses = response.data;
+      });
+    },
+    playCourse(id) {
+      Course.getCoursesById(id).then((response) => {
+        this.currentCourse = response.data;
+        this.currentSection = this.currentCourse.sections[0];
+        const currentMovie = this.currentSection.contents[0].value;
+        this.$router.push({
+          path: "/classroom",
+          query: {
+            courseId: id,
+            currentMovie: currentMovie,
+          },
+        });
+      });
+    },
+  },  
 }
 </script>
